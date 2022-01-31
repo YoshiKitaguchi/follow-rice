077374be93130a8ab7152cf75234af4a9ff3e79f
YoshiKitaguchi <ykita0224@gmail.com>
2022-01-31 18:49:18 -0500

    added images

--- src/components/Homepage/ImageGallery/ImageGallery.js
+++ src/components/Homepage/ImageGallery/ImageGallery.js
@@ -1,21 +1,43 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './ImageGallery.css';
 import ImageGallery from 'react-image-gallery';
 
+import s1_1 from '../../image/s1_1.jpg';
+import s1_2 from '../../image/s1_2.jpg';
+import s1_3 from '../../image/s1_3.jpg';
+import s1_4 from '../../image/s1_4.jpg';
+import s1_5 from '../../image/s1_5.jpg';
+
 
 const useImageGallery = () => {
+
+
+    const orignal_height = '500px';
     const images = [
         {
-            original: 'https://picsum.photos/id/1018/1000/600/',
-            thumbnail: 'https://picsum.photos/id/1018/250/150/',
+            original: s1_1,
+            originalHeight: orignal_height,
+            thumbnail: s1_1,
         },
         {
-            original: 'https://picsum.photos/id/1015/1000/600/',
-            thumbnail: 'https://picsum.photos/id/1015/250/150/',
+            original: s1_2,
+            thumbnail: s1_2,
+            originalHeight: orignal_height,
         },
         {
-            original: 'https://picsum.photos/id/1019/1000/600/',
-            thumbnail: 'https://picsum.photos/id/1019/250/150/',
+            original: s1_3,
+            thumbnail: s1_3,
+            originalHeight: orignal_height,
+        },
+        {
+            original: s1_4,
+            thumbnail: s1_4,
+            originalHeight: orignal_height,
+        },
+        {
+            original: s1_5,
+            thumbnail: s1_5,
+            originalHeight: orignal_height,
         },
     ];
 
--- src/components/Homepage/StoreList/StoreList.js
+++ src/components/Homepage/StoreList/StoreList.js
@@ -4,6 +4,7 @@ import b1 from '../../image/bento1.jpg';
 // import b2 from '../../image/bento2.jpg';
 // import b3 from '../../image/bento3.jpg';
 import CustomPopup from '../CustomPopup/CustomPopup';
+// import  Text  from "react-native";
 
 
 const useStoreList = () => {
@@ -23,14 +24,23 @@ const useStoreList = () => {
                 <CustomPopup
                     onClose={popupCloseHandler}
                     show={visibility}
-                    title="何て名前だったっけ？"
+                    title="Bar Laugh Tale"
                 >
-                    <h2>【高円寺駅 南口徒歩30秒】【キャンペーン中】</h2>
-                    <h2> 夜景が楽しめるオシャレなbar </h2>
-                    <h2> 東京都杉並区高円寺南４-２７-１０ タイガーデン４階 </h2>
-                    <h2> ゆったりとした空間が広がりその中でカラオケや気さくな店員さんとゆっくり飲めるbarです。</h2>
-                    <h2> デートやしっぽり飲みたい人にはとてもぴったりでカラオケが無料でついてるのでさまざまな楽しみ方でたのしめます。</h2>
-                    <h2> お一人様でも大歓迎 </h2>
+                    <h3 className={classes.text}>
+                        {`
+                            【高円寺駅 南口徒歩30秒】【キャンペーン中】
+
+                            夜景が楽しめるオシャレなbar
+
+                            東京都杉並区高円寺南４-２７-１０ タイガーデン４階
+
+                            ゆったりとした空間が広がりその中でカラオケや気さくな店員さんとゆっくり飲めるbarです。
+
+                            デートやしっぽり飲みたい人にはとてもぴったりでカラオケが無料でついてるのでさまざまな楽しみ方でたのしめます。
+
+                            お一人様でも大歓迎
+                        `}
+                    </h3>
                 </CustomPopup>
             </div>
         </div>
